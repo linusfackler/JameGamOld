dd6cfcbbce4c2cdfabf553b80f131bf68e2c140b
linusfackler <78768883+linusfackler@users.noreply.github.com>
2022-08-06 00:32:39 +0200

    added movement script

---
+++ Assets/Scipts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public Vector2 startPos;
+
+    private float horizontal;
+    public float speed;
+    public bool isFacingRight = true;
+
+    void Start()
+    {
+        //transform.position = startPos;
+    }
+
+    void Update()
+    {
+        if (!isFacingRight && horizontal > 0f)
+            Flip();
+        else if (isFacingRight && horizontal < 0f)
+            Flip();
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+    }
+
+        private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.Rotate(0f, 180f, 0f);
+    }
+}
